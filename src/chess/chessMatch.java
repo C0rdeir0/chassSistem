fdd783b76a1d4f0cf639b0a7a374b3f90145c20d
Ewerton Cordeiro <ewertonecordeiro@gmail.com>
2022-08-20 09:15:11 -0300

    Placing pieces onthe board

--- src/chess/chessMatch.java
+++ src/chess/chessMatch.java
@@ -1,6 +1,11 @@
 package chess;
 
 import boardGame.Board;
+import boardGame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
+
+
 
 public class chessMatch {
 	
@@ -8,6 +13,7 @@ public class chessMatch {
 	
 	public chessMatch() {
 		board = new Board(8,8); 
+		initialSetup();
 	}
 	public chessPiece [][] getPieces(){
 		chessPiece[][] mat= new chessPiece [board.getRows()][board.getColumns()];
@@ -18,6 +24,10 @@ public class chessMatch {
 		}
 		return mat;
 	}
-	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE),new Position(2,1));
+		board.placePiece(new King(board, Color.BLACK),new Position(0,4));
+		board.placePiece(new King(board, Color.WHITE),new Position(7,4));
+	}
 
 }
---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardGame.Board;
+import chess.Color;
+import chess.chessMatch;
+import chess.chessPiece;
+
+public class Rook extends chessPiece {
+
+	public Rook(Board board, Color color) {
+		super(board, color);
+		
+	}
+
+	@Override
+	public String toString() {
+		return "R";
+	}
+	
+
+}
